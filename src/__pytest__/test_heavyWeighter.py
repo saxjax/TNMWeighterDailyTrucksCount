0a31739069b8d4e23c92ab061f9ada0b68e1c773
jchri17 <58163688+jchri17@users.noreply.github.com>
2021-12-08 21:22:02 +0100

    Test Endpoint
    pt laver den selv endpoints også tester  om de kan oprettes med givent input enten strings eller json den bruger ikke service.py filen pt

--- src/__pytest__/test_heavyWeighter.py
+++ src/__pytest__/test_heavyWeighter.py
@@ -1,50 +1,50 @@
-import json
-import joblib
-import pandas as pd
-import pytest
-from src.enhancer_microservice import Enhancer
-from src.heavy_weighter import predict_heavy_traffic, train_model_on_TNM
-from src.__pytest__.conftest import fake_json
+# import json
+# import joblib
+# import pandas as pd
+# import pytest
+# from src.enhancer_microservice import Enhancer
+# from src.heavy_weighter import predict_heavy_traffic, train_model_on_TNM
+# from src.__pytest__.conftest import fake_json
 
-def test_that_predicter_predicts_dailyTrucks (fake_json,fake_dataframe):
-    #arrange
-    pre_dataframe = pd.DataFrame(fake_dataframe)
+# def test_that_predicter_predicts_dailyTrucks (fake_json,fake_dataframe):
+#     #arrange
+#     pre_dataframe = pd.DataFrame(fake_dataframe)
     
-    before_predictions_on_daily_trucks = pre_dataframe['daily_trucks'].tolist()
+#     before_predictions_on_daily_trucks = pre_dataframe['daily_trucks'].tolist()
     
-    #prepare for predicter, by removing theese columns
-    pre_dataframe_dropped_columns = pre_dataframe.drop(columns=['max_axle_load', 'max_height','max_weight', 'max_length', 'recommended_speed', 'mean_speed','daily_trucks'])
+#     #prepare for predicter, by removing theese columns
+#     pre_dataframe_dropped_columns = pre_dataframe.drop(columns=['max_axle_load', 'max_height','max_weight', 'max_length', 'recommended_speed', 'mean_speed','daily_trucks'])
 
-    #the trained model found in the filesystem
-    model = joblib.load('decision_tree_model.joblib')
+#     #the trained model found in the filesystem
+#     model = joblib.load('decision_tree_model.joblib')
     
 
-    #act
-    predicted_dataframe = model.predict(pre_dataframe_dropped_columns)
-    after_predictions_on_daily_trucks = predicted_dataframe.tolist()
-    with pd.option_context('display.max_rows', None, 'display.max_columns', pre_dataframe.shape[1]):
-        print("dataframe used for predictions:\n",pre_dataframe)
-        print("the  daily_trucks before predictions:\n",before_predictions_on_daily_trucks)
-        print("the predicted daily_trucks after predictions:\n",predicted_dataframe)
-    #assert
-    assert after_predictions_on_daily_trucks != before_predictions_on_daily_trucks
+#     #act
+#     predicted_dataframe = model.predict(pre_dataframe_dropped_columns)
+#     after_predictions_on_daily_trucks = predicted_dataframe.tolist()
+#     with pd.option_context('display.max_rows', None, 'display.max_columns', pre_dataframe.shape[1]):
+#         print("dataframe used for predictions:\n",pre_dataframe)
+#         print("the  daily_trucks before predictions:\n",before_predictions_on_daily_trucks)
+#         print("the predicted daily_trucks after predictions:\n",predicted_dataframe)
+#     #assert
+#     assert after_predictions_on_daily_trucks != before_predictions_on_daily_trucks
 
-# @pytest.mark.parametrize('tnm',[
-# (fake_json),
-# ])
-# def test_that_predicter_only_predicts_on_undefined_data (tnm):
-#     # kun værdier som ikke allerede findes i data må predictes.    
-#     # # arrange 
+# # @pytest.mark.parametrize('tnm',[
+# # (fake_json),
+# # ])
+# # def test_that_predicter_only_predicts_on_undefined_data (tnm):
+# #     # kun værdier som ikke allerede findes i data må predictes.    
+# #     # # arrange 
     
-#     # act
-#     actual = predict_heavy_traffic(tnm)
+# #     # act
+# #     actual = predict_heavy_traffic(tnm)
 
 
-#     # assert
-#     assert True
+# #     # assert
+# #     assert True
 
-# def test_that_prediction_is_above_0(tnm):
-#     assert True
+# # def test_that_prediction_is_above_0(tnm):
+# #     assert True
 
-# def test_that_prediction_is_below_max_daily_trucks(tnm):
-#     assert True
+# # def test_that_prediction_is_below_max_daily_trucks(tnm):
+# #     assert True
--- src/__pytest__/test_service.py
+++ src/__pytest__/test_service.py
@@ -16,58 +16,66 @@ from src.__pytest__.conftest import client
 # def test_convert(fake_json):
 #     print(from_json(fake_json))
 
-def test_json_client(httpserver: HTTPServer):
-    httpserver.expect_request("/info").respond_with_data("OK")
+def test_info(httpserver: HTTPServer):
+    httpserver.expect_request("/info").respond_with_data("heavyweighter /info")
     assert requests.get(httpserver.url_for("/info")).status_code == 200#### test hvad der kommer tilbage i forhold til service.py filen
 
     #httpserver.expect_request("/foobar").respond_with_json({"foo": "bar"})
     #assert requests.get(httpserver.url_for("/foobar")).json() == {'foo': 'bar'}
-def test_info():
-   # service.run(host="0.0.0.0", port=5000)
+
+def test_readme(httpserver: HTTPServer):
+    httpserver.expect_request("/readme").respond_with_data("readme file")
+    assert requests.get(httpserver.url_for("/readme")).status_code == 200#### test hvad der kommer tilbage i forhold til service.py filen
+
+def test_data(httpserver: HTTPServer):
+    httpserver.expect_request("/data").respond_with_json({"foo": "bar"})
+    assert requests.get(httpserver.url_for("/data")).json() == {'foo': 'bar'}
+# def test_info():
+#    # service.run(host="0.0.0.0", port=5000)
     
-    response = request.get('https://localhost/info')
-    # assert response.status_code == 200
+#     response = request.get('https://localhost/info')
+#     # assert response.status_code == 200
     
-    assert True
+#     assert True
 
-def test_readme():
-    response = request.get('https://localhost/readme')
-    # assert response.status_code == 200
+# def test_readme():
+#     response = request.get('https://localhost/readme')
+#     # assert response.status_code == 200
     
-    assert True
+#     assert True
 
-def test_data(mocker):
-    mock = mocker.patch("src.model_training.joblib", return_value=None)
+# def test_data(mocker):
+#     mock = mocker.patch("src.model_training.joblib", return_value=None)
 
-    response = request.get('https://localhost/data')
-    # assert response.status_code == 200
+#     response = request.get('https://localhost/data')
+#     # assert response.status_code == 200
     
-    # mock.dump.assert_called_once()
+#     # mock.dump.assert_called_once()
 
-    assert True
+#     assert True
 
-# @pytest.mark.parametrize('endpoint',[
-# ('/info'),
-# ('/data'),
-# ('/readme')
-# ])
-def test_that_service_can_call_endpoint(mocker,endpoint):
-    # arrange 
-    mock = mocker.patch("src.model_training.joblib", return_value=None)
+# # @pytest.mark.parametrize('endpoint',[
+# # ('/info'),
+# # ('/data'),
+# # ('/readme')
+# # ])
+# def test_that_service_can_call_endpoint(mocker,endpoint):
+#     # arrange 
+#     mock = mocker.patch("src.model_training.joblib", return_value=None)
 
-    url = 'localhost:5000/'+endpoint
-    expectedStatuscode = 200
-    # mock.dump.assert_called_once()
+#     url = 'localhost:5000/'+endpoint
+#     expectedStatuscode = 200
+#     # mock.dump.assert_called_once()
 
 
-    # https://stackoverflow.com/questions/57663308/how-to-mock-requests-using-pytest
-    # https://www.freecodecamp.org/news/end-to-end-api-testing-with-docker/
+#     # https://stackoverflow.com/questions/57663308/how-to-mock-requests-using-pytest
+#     # https://www.freecodecamp.org/news/end-to-end-api-testing-with-docker/
     
 
-    # act
+#     # act
 
-    # assert 
-    assert True
+#     # assert 
+#     assert True
 
 # @pytest.mark.parametrize('endpoint',[
 # ('/info'),
